8450be63675b310b76b9dbbb8444bafa7c3d9ec1
Bharat Kunwar <brtknr@bath.edu>
2019-10-21 11:53:53 +0100

    Support default_retention_hours for influxdb
    At the moment, all data is retained infinitely. This change allows users
    to specify a default_retention_hours (defaults to 0 meaning unlimited
    retention) in the influxdb section of monasca persister configuration so
    that new projects are automatically assigned this retention policy when
    the given hour is greater than 0 to preserve original behaviour.
    
    Story: 2006331
    Task: 37234
    
    Change-Id: I4136df1d43954eb026a104f3f85b3a58197f5435
    (cherry picked from commit b3a30d5516a2c3e4debba29e9a348f47d949ecd3)
    
    Conflicts:
    	monasca_persister/tests/test_influxdb_metrics_repository.py

--- monasca_persister/conf/influxdb.py
+++ monasca_persister/conf/influxdb.py
@@ -23,6 +23,9 @@ influxdb_opts = [
     cfg.BoolOpt('db_per_tenant',
                 help='Whether to use a separate database per tenant',
                 default=False),
+    cfg.IntOpt('default_retention_hours',
+               help='Default retention period in hours',
+               default=0),
     cfg.HostAddressOpt('ip_address',
                        help='Valid IP address or hostname '
                             'to InfluxDB instance'),
